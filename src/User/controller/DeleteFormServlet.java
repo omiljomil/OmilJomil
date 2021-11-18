db4c3c9fc4fa5aa92df15fb59a3b801ea94ffca8
LeeHeeYeong <minnaok1@gmail.com>
2021-11-18 09:42:07 +0900

    commit

--- src/User/controller/DeleteFormServlet.java
+++ src/User/controller/DeleteFormServlet.java
@@ -7,10 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import User.model.service.UserService;
-import User.model.vo.User;
-
-
 /**
  * Servlet implementation class DeleteFormServlet
  */
