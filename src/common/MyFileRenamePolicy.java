0c982b5d87c050721b2c730fb6fd26f07416c295
zoon1031 <zoon1031@naver.com>
2021-11-18 19:19:50 +0900

    충돌??;;

--- src/common/MyFileRenamePolicy.java
+++ src/common/MyFileRenamePolicy.java
@@ -6,16 +6,11 @@ import java.util.Date;
 
 import com.oreilly.servlet.multipart.FileRenamePolicy;
 
-<<<<<<< HEAD
 public class MyFileRenamePolicy implements FileRenamePolicy{
-=======
-public class MyFileRenamePolicy implements FileRenamePolicy {
->>>>>>> ab7e767dd5b814f4aa3e71bc4a2d6612d607bfba
 
 	@Override
 	public File rename(File originFile) {
 		
-<<<<<<< HEAD
 		long currentTime=System.currentTimeMillis();//현재시간을 millisceond형태로 가져와서 변수에 담는다!
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmssSSS");
 		int ranNum=(int)(Math.random()*100000);
@@ -38,30 +33,4 @@ public class MyFileRenamePolicy implements FileRenamePolicy {
 	}
 
 }
-=======
-	
-		long currentTime = System.currentTimeMillis();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-		int ranNum = (int)(Math.random()*100000);
-		
-		String name = originFile.getName();
-		String ext = null;
-		
-		int dot = name.lastIndexOf(".");
-		if(dot != -1) {
-			ext = name.substring(dot);		
-		}else {
-			ext = "";
-		}
-		
-		String fileName = sdf.format(new Date(currentTime)) + ranNum + ext;
-		File newFile = new File(originFile.getParent(), fileName);
-		
-		return newFile;
-	}
-	
-	
 
-}
-
->>>>>>> ab7e767dd5b814f4aa3e71bc4a2d6612d607bfba
